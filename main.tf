2e7fbeda46ab7d69012487b82a07b78d5f6ab5d3
Gabriel Rodrigues <gabrieloliveira12488@gmail.com>
2023-03-22 17:40:22 -0300

    feat: switching location to East US

--- main.tf
+++ main.tf
@@ -22,14 +22,38 @@ resource "azurerm_resource_group" "ipfs" {
 }
 
 
-module "powergate-machine" {
+module "powergate_machine" {
   source              = "./modules/powergate-filecoin"
   resource_group_name = azurerm_resource_group.ipfs.name
-  location = "East US"
+  location            = "East US"
 }
 
 
+resource "time_sleep" "wait_15_minutes" {
+  depends_on = [module.powergate_machine.instance_ip_addr]
+
+  create_duration = "15m"
+}
+
+resource "null_resource" "cluster" {
+  depends_on = [
+    time_sleep.wait_15_minutes
+  ]
+  # node ./scripts/set-token/set-token.js
+  provisioner "local-exec" {
+    command = "node ./scripts/set-token/set-token.js http://${module.powergate_machine.instance_ip_addr}:6002"
+    
+    
+  }
+}
 
 
-
-
+module "powergate-rest-middleware" {
+  depends_on = [
+    null_resource.cluster
+  ]
+  source              = "./modules/file-uploader-middleware"
+  resource_group_name = "bridgez"
+  location            = "East US"
+  powergate_ip = module.powergate_machine.instance_ip_addr
+}
