27798b9d75118cc8cd52e9e7d894f6dd3db692e5
Gabriel Rodrigues <gabrieloliveira12488@gmail.com>
2023-03-22 17:37:37 -0300

    feat: installation of docker-compose

--- scripts/script.sh
+++ scripts/script.sh
@@ -14,6 +14,11 @@ sudo usermod -aG docker ${USER}
 sudo usermod -aG docker adminuser
 
 
+#Installation of docker-compose
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 #Installation of Golang
 sudo wget https://go.dev/dl/go1.18.5.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.18.5.linux-amd64.tar.gz
@@ -32,4 +37,24 @@ cd /home/adminuser
 
 git clone https://github.com/textileio/powergate
 
+# cd powergate/docker 
+
+# make localnet
+
+
+#Instalation of IPFS
+
+wget https://dist.ipfs.tech/kubo/v0.15.0/kubo_v0.15.0_linux-amd64.tar.gz
+tar -xvzf kubo_v0.15.0_linux-amd64.tar.gz
+sudo bash kubo/install.sh
+
+ipfs init
+
+ipfs config Addresses.API /ip4/0.0.0.0/tcp/5001
+ipfs config Addresses.Gateway /ip4/0.0.0.0/tcp/9001
+
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["*"]'
+ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'
+
+ipfs daemon &
 
